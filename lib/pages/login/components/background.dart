8398fe7d8fb177bf8e2fc4efc13a9ff6424cd776
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-06-16 14:46:14 +0530

    login screen added

---
+++ lib/pages/login/components/background.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  final String topImage;
+  final String bottomImage;
+
+  const Background({
+    Key? key,
+    required this.child,
+    this.topImage = "assets/images/main_top.png",
+    this.bottomImage = "assets/images/login_bottom.png",
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SizedBox(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                topImage,
+                width: 120,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                bottomImage,
+                width: 120,
+              ),
+            ),
+            SafeArea(
+              child: child,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login/components/login_form.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: "Your email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: "Your password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Login".toUpperCase(),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+        ],
+      ),
+    );
+  }
+}
