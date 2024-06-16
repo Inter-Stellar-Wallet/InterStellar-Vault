075785cb86c21447f1103bf4f7c85f25c3dbff10
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-06-16 22:41:57 +0530

    Added Name

--- lib/pages/my_qr.dart
+++ lib/pages/my_qr.dart
@@ -7,91 +7,138 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-class MyQrCode extends StatelessWidget {
-
+class MyQrCode extends StatefulWidget {
   const MyQrCode({Key? key}) : super(key: key);
 
+  @override
+  State<MyQrCode> createState() => _MyQrCodeState();
+}
+
+class _MyQrCodeState extends State<MyQrCode> {
+  String phone = "";
+  String email = "";
+
+  @override
+  void initState() {
+    getDetails();
+    super.initState();
+  }
+
+  void getDetails() {
+    final users = context.read<LoggedInStore>().users;
+    final accountId = StellarHelper.accountData!.accountId;
+
+    for (var user in users) {
+      if (user.accountId == accountId) {
+        setState(() {
+          phone = user.phone;
+          email = user.email;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text('My Qr Code', style: TextStyle(color: Colors.black),),
-        leading: BackButton(color: Colors.black,),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 50,),
-              FadeInDown(
-                from: 100,
-                duration: Duration(milliseconds: 1000),
-                child: Container(
-                  width: 130,
-                  height: 130,
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.pink.shade50,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Image.asset("assets/images/avatar-1.png")),
-                ),
-              ),
-              SizedBox(height: 15,),
-              FadeInUp(
-                from: 60,
-                delay: Duration(milliseconds: 500),
-                duration: Duration(milliseconds: 1000),
-                child: Text("Hello I am", style: TextStyle(color: Colors.grey),)),
-              SizedBox(height: 10,),
-              FadeInUp(
-                from: 30,
-                delay: Duration(milliseconds: 800),
-                duration: Duration(milliseconds: 1000),
-                child: Text("Jhon", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),)),
-              SizedBox(height: 40,),
-              FadeInUp(
-                from: 30,
-                delay: Duration(milliseconds: 800),
-                duration: Duration(milliseconds: 1000),
-                child: Observer(
-                  builder: (_) {
-                    return QrImageView(
-                      data: StellarHelper.keyPair0!.accountId.toString(),
-                      version: QrVersions.auto,
-                      size: 300.0,
-                    );
-                  }
-                ),
-                
-              ),
-              SizedBox(height: 40,),
-              FadeIn(
-                duration: const Duration(milliseconds: 500),
-                child: MaterialButton(
-                  height: 60,
-                  padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-                  onPressed: () {},
-                  child: const Text('Share', style: TextStyle(color: Colors.black, fontSize: 15),),
-                  color: Colors.transparent,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.grey.shade300, width: 1),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ),
-            ]
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            'My Qr Code',
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: BackButton(
+            color: Colors.black,
           ),
         ),
-      )
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FadeInDown(
+                    from: 100,
+                    duration: Duration(milliseconds: 1000),
+                    child: Container(
+                      width: 130,
+                      height: 130,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.pink.shade50,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.asset("assets/images/avatar-1.png")),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  FadeInUp(
+                      from: 60,
+                      delay: Duration(milliseconds: 500),
+                      duration: Duration(milliseconds: 1000),
+                      child: Text(
+                        "Hello I am",
+                        style: TextStyle(color: Colors.grey),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FadeInUp(
+                      from: 30,
+                      delay: Duration(milliseconds: 800),
+                      duration: Duration(milliseconds: 1000),
+                      child: Text(
+                        email,
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  FadeInUp(
+                    from: 30,
+                    delay: Duration(milliseconds: 800),
+                    duration: Duration(milliseconds: 1000),
+                    child: Observer(builder: (_) {
+                      return QrImageView(
+                        data: StellarHelper.keyPair0!.accountId.toString(),
+                        version: QrVersions.auto,
+                        size: 300.0,
+                      );
+                    }),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  FadeIn(
+                    duration: const Duration(milliseconds: 500),
+                    child: MaterialButton(
+                      height: 60,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 80, vertical: 10),
+                      onPressed: () {},
+                      child: const Text(
+                        'Share',
+                        style: TextStyle(color: Colors.black, fontSize: 15),
+                      ),
+                      color: Colors.transparent,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade300, width: 1),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
+        ));
   }
 }
